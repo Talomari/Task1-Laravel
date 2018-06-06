c762b0cbb34633da11fdf17730dd8d85e8b4de03
TalOmari <talomari592@gmail.com git config --global user.email talomari592@gmail.com>
2018-06-06 23:08:03 +0300

    task1 laravel php

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+     
+       -webkit-background-size: cover;
+        -moz-background-size: cover;
+       -o-background-size: cover;
+        background-size: cover;
+        }
+        input{
+         width:500px;
+        }
+        opacity: 0.5;
+        background-color: white;
+        height: 20px;
+        }
+        </style>
+    </head>
+    <body background="http://www.cbc.ca/doczone/content/images/episodes/waethergonewiild_1280.jpg">
+
+            <div class="content">
+                <h1>{{$data}}
+                
+               </h1>
+                
+    </body>
+</html>
